3efbae2eb294f2a8cb379707d9a469aff1ea64ac
jhongonzalezs <jhongonzalezs2001@hotmail.com>
2025-07-07 10:02:55 -0500

    agregar los cors en order

--- order_service/main.py
+++ order_service/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .models import OrderItem
 from .routes import order 
+from fastapi.middleware.cors import CORSMiddleware 
 
 Base.metadata.create_all(bind=engine, tables=[OrderItem.__table__])
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(order.router, prefix="", tags=["Ordenes"])
 
 @app.get("/")
